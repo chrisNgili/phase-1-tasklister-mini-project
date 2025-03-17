6cbce834e6b912282c7f59bf20b54da2bdb57b22
chrisngili <ngilichristopher@gmail.com>
2025-03-17 11:55:19 +0300

    project complete again

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-  let form = document.querySelector('form')
+  let form = document.querySelector('#create-task-form'); 
   form.addEventListener('submit', (e) => {
-    e.preventDefault() 
-    buildToDo(e.target.new_todo.value)
-    form.reset()
-    document.getElementById('date').innerHTML = new Date().toDateString()
-  })
+    e.preventDefault();
+    
+    let input = document.querySelector('#new-task-description'); 
+    buildToDo(input.value);
 
+    document.getElementById('date').innerHTML = new Date().toDateString();
+    form.reset();
+  });
 });
-function buildToDo(todo){
-  let li = document.createElement('li')
-  let btn = document.createElement('button')
-  btn.addEventListener('click', handleDelete)
-  btn.className = 'botton'
-  btn.textContent = 'x'
-  li.textContent = `${todo}  `
-  li.appendChild(btn)
-  document.querySelector('#tasks').appendChild(li)
-  
+
+function buildToDo(todo) {
+  let li = document.createElement('li');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', handleDelete);
+  btn.className = 'botton';
+  btn.textContent = 'x';
+  li.textContent = `${todo}  `;
+  li.appendChild(btn);
+  document.querySelector('#tasks').appendChild(li);
 }
-function handleDelete(e){
-  e.target.parentNode.remove()
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
 }
